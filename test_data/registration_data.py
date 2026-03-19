6a60814d8bf013bbe91ca0566dd9dea6a4b4eb41
Sylwia95 <sylwia407@onet.pl>
2026-03-19 18:24:20 +0000

    Add registration tests

---
+++ test_data/registration_data.py
@@ -0,0 +1,16 @@
+from faker import Faker
+
+
+class RegistrationDataGenerator:
+    def __init__(self):
+        self.fake = Faker("pl_PL")
+        self.first_name = self.fake.first_name()
+        self.last_name = self.fake.last_name()
+        self.address = self.fake.street_address()
+        self.city = self.fake.city()
+        self.state = self.fake.state()
+        self.zip_code = self.fake.postcode()
+        self.phone = self.fake.phone_number()
+        self.pesel = self.fake.ssn()
+        self.username = self.fake.user_name()
+        self.password = self.fake.password()
---
+++ tests/registration_test.py
@@ -0,0 +1,22 @@
+from test_data.registration_data import  RegistrationDataGenerator
+from tests.main_test import MainTest
+from pages.register_page import RegisterPage
+
+
+class RegistrationTest(MainTest):
+    def setUp(self):
+        super().setUp()
+        self.main_page.click_register_button()
+        self.register_page = self.main_page.click_register_button()
+
+     def testcompleteform (self):
+         self.register_page.enter_first_name(self.data.first_name)
+         self.register_page.enter_last_name(self.data.last_name)
+         self.register_page.enter_address(self.data.address)
+         self.register_page.enter_city(self.data.city)
+         self.register_page.enter_state(self.data.state)
+         self.register_page.enter_zip_code(self.data.zip_code)
+         self.register_pagge.enter_phone(self.data.phone)
+         self.register_page.enter_username(self.data.username)
+         self.register_page.enter_password(self.data.password)
+         self.register_page.enter_confirm_password(self.data.password)
